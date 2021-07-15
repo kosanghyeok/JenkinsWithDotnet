9f732b2908c7f511057ae3c9d01c7c48b791aef1
sanghyeok ko <kosang5719@gmail.com>
2021-07-16 02:17:51 +0900

    3rd

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace JenkinsWithDotnet
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello SangSangHyeok 222!! ^^ !");
+                    await context.Response.WriteAsync("Hello SangSangHyeok 3333!! ^^ !");
                 });
             });
         }
